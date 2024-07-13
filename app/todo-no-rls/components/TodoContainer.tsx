78d51f48c1b27f487a7b53f0f1ff454f2fbf5d68
jwkim\Administrator <a>
2024-07-13 10:18:36 +0900

    클라이언트

--- app/todo-no-rls/components/TodoContainer.tsx
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -1,19 +1,33 @@
 "use client";
-import {
-  getTodos,
-  getTodosById,
-  getTodosBySearch,
-  createTodos,
-  deleteTodosSoft,
-  updateTodos,
-} from "@/apis/totos-no-rls";
 import React, { useEffect } from "react";
+import useTodosController from "../hooks/useTodosController";
+import TodoList from "@/components/ui/TodoList";
 
 const TodoContainer = () => {
-  useEffect(() => {
-    getTodos();
-  }, []);
-  return <div>TodoContainer</div>;
+  const {
+    loading,
+    todos,
+    onCreateEmptyTodos,
+    onDeleteTodos,
+    onSearchTodos,
+    onUpdateTodos,
+  } = useTodosController();
+
+  return (
+    <div>
+      <TodoList
+        sharedUserFullName="test user"
+        owerUserId="123123"
+        loading={loading}
+        todoListData={todos}
+        isReadOnly={false}
+        onUpdate={onUpdateTodos}
+        onCreate={onCreateEmptyTodos}
+        onDelete={onDeleteTodos}
+        onSearch={onSearchTodos}
+      />
+    </div>
+  );
 };
 
 export default TodoContainer;
--- app/todo-no-rls/page.tsx
+++ app/todo-no-rls/page.tsx
@@ -1,9 +1,9 @@
 import { sleep } from "@/lib/utils";
 import React from "react";
 import TodoContainer from "./components/TodoContainer";
+import useTodosController from "./hooks/useTodosController";
 
 const page = async () => {
-  await sleep(1500);
   return (
     <div>
       <TodoContainer />
