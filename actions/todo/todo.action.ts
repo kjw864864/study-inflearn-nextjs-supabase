b9c83e153684588d316c20ec97ef6eb453e3c1b3
jwkim\Administrator <a>
2024-07-13 15:27:35 +0900

    커밋

---
+++ actions/todo/todo.action.ts
@@ -0,0 +1,84 @@
+"use server";
+import { createServerSideClient } from "@/lib/supabase";
+
+export const getTodos = async () => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .order("id", { ascending: false });
+
+  return result.data;
+};
+
+export const getTodosById = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id);
+  return result.data;
+};
+
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}%`)
+    .order("id", { ascending: false });
+  return result.data;
+};
+
+// todoList 생성하기
+export const createTodos = async (content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .insert({
+      content,
+    })
+    .select();
+
+  return result.data;
+};
+
+// todoList 업데이트 하기
+export const updateTodos = async (id: number, content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({
+      content,
+      updated_at: new Date().toISOString(),
+    })
+    .eq("id", id)
+    .select();
+
+  return result.data;
+};
+
+// todoList softDelete
+export const deleteTodosSoft = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({
+      deleted_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    })
+    .eq("id", id)
+    .select();
+
+  return result.data;
+};
+
+// todoList hardDelete
+// export const deleteTodosHard = async (id: number) => {
+//   const supabase = await createServerSideClient();
+//   const result = await supabase.from("todos_with_rls").delete().eq("id", id);
+//   return result.data;
+// };
---
+++ app/auth/callback/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { createServerSideClient } from "@/lib/supabase";
+
+export async function GET(request: Request) {
+  const overrideOrigin = process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME;
+  const { searchParams, origin } = new URL(request.url);
+
+  const code = searchParams.get("code");
+  const next = searchParams.get("next");
+  console.log("code", code);
+  console.log("next", next);
+  if (code) {
+    const supabase = await createServerSideClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code); //토큰을 교환하는 함수
+    if (error) return NextResponse.redirect(`${overrideOrigin}`);
+
+    return NextResponse.redirect(`${overrideOrigin}${next}`);
+  }
+  return NextResponse.redirect(`${overrideOrigin}`);
+}
